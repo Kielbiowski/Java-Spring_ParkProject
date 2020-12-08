991e99e779056555fca08eda48e703d9b12393fc
Szymon Kiełbiowski <kielbiowski.main@gmail.com>
2020-12-08 16:27:11 +0100

    basic spring initializr project initialized

---
+++ park-project/src/main/java/com/kielbiowski/parkproject/ParkProjectApplication.java
@@ -0,0 +1,13 @@
+package com.kielbiowski.parkproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkProjectApplication.class, args);
+	}
+
+}

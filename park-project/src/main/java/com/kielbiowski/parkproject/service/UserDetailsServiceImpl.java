9563198f5a8e3ca6fafa0fb6cf5ffe553fd725b5
Kielbiowski <kielbiowski.main@gmail.com>
2021-01-08 14:37:25 +0100

    role repository created, user repository modified. UserDetailsServiceImpl service created

---
+++ park-project/src/main/java/com/kielbiowski/parkproject/service/UserDetailsServiceImpl.java
@@ -0,0 +1,37 @@
+package com.kielbiowski.parkproject.service;
+
+import com.kielbiowski.parkproject.exception.NotFoundException;
+import com.kielbiowski.parkproject.model.Role;
+import com.kielbiowski.parkproject.model.User;
+import com.kielbiowski.parkproject.repository.UserRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) {
+        User user = userRepository.findByEmail(username);
+        if (user==null) throw new NotFoundException();
+
+        Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
+        for (Role role : user.getRoles()){
+            grantedAuthorities.add(new SimpleGrantedAuthority(role.getName()));
+        }
+
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), grantedAuthorities);
+    }
+}
